b10f04ef3542ef2d8843cec2ac67366ff8c2b28e
Tri Pham <tripham@Tris-MacBook-Pro.local>
2026-02-10 21:32:09 -0600

    adjust envi 1

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   process.env.NEXT_PUBLIC_SOCKET_URL ||
-  "http://localhost:3001";
+  "https://duo-bugj.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
